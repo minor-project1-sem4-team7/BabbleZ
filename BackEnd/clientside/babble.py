0ffcd5181e306dbb262233bdce4317f928646d0c
ohh-sorry-i-am-a-noobie <ohh-sorry@i-am-a-noob.ie>
2021-03-23 10:19:50 +0530

    messg

---
+++ BackEnd/clientside/babble.py
@@ -0,0 +1,13 @@
+import requests
+import threading
+import socket
+
+class Babble:
+    def __init__(self):
+        pass
+    def send_messg(self):
+        pass
+    def recieve_messg(self):
+        pass
+    def save_messg(self):
+        pass
