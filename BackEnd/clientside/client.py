4ac85bcd930644c6e14254aaa40ad1ff778758e9
ohh-sorry-i-am-a-noobie <ohh-sorry@i-am-a-noob.ie>
2021-03-25 14:36:34 +0530

    save progress

--- BackEnd/clientside/client.py
+++ BackEnd/clientside/client.py
@@ -1,9 +1,9 @@
 import threading
 import socket
-
+from datetime import datetime
+from user import User
 
 def log(typ: str, text: str):
-
     '''
     [!] Information\n
     [#] Important or Warning\n
@@ -13,12 +13,12 @@ def log(typ: str, text: str):
     [N] Count
     '''
 
-    from datetime import datetime
+
     timestamp = datetime.now()
 
     typ = '[' + typ + ']'
-    with open('logfile.txt','a') as file:
-        file.write(typ + ' '+str(timestamp) + ' ' +text+'\n')
+    with open('logfile.txt', 'a') as file:
+        file.write(typ + ' ' + str(timestamp) + ' ' + text + '\n')
 
 
 class Babble:
@@ -27,22 +27,22 @@ class Babble:
         self.port = 27526
         self.ip = '127.0.0.1'
         self.is_connected = False
-        log('!',f'Server IP {self.ip}')
-        log('!',f'Server PORT {self.port}')
+        log('!', f'Server IP {self.ip}')
+        log('!', f'Server PORT {self.port}')
 
     def connect(self):
         try:
             self.socket.connect((self.ip, self.port))
         except:
             trial = 3
-            while(trial):
-                log(f'{4-trial}','Trying Again')
+            while trial:
+                log(f'{4 - trial}', 'Trying Again')
                 trial -= 1
                 try:
-                    log('.','Connecting...')
+                    log('.', 'Connecting...')
                     self.socket.connect((self.ip, self.port))
                 except:
-                    log('-','Failed To connect!!!')
+                    log('-', 'Failed To connect!!!')
 
     def send_messg(self):
         pass
@@ -52,6 +52,12 @@ class Babble:
 
     def save_messg(self):
         pass
+
+    def status_report(self, userid):
+
+        packet = '{} {}'.format(userid, datetime.now())
+        while True:
+            self.socket.send(packet)
 
 
 if __name__ == '__main__':
