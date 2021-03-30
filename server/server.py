d44686a4be1189d3d8f944dd934882f08710d7e7
ohh-sorry-i-am-a-noobie <ohh-sorry@i-am-a-noob.ie>
2021-03-30 21:47:48 +0530

    progress save 2.3.0

--- server/server.py
+++ server/server.py
@@ -1,9 +1,10 @@
 import threading
 import socket
+import pickle
 from server_security import Security
 
-def log(typ: str, text: str):
 
+def log(typ: str, text: str):
     '''
     [!] Information\n
     [#] Important or Warning\n
@@ -17,37 +18,68 @@ def log(typ: str, text: str):
     timestamp = datetime.now()
 
     typ = '[' + typ + ']'
-    with open('logfile.txt','a') as file:
-        file.write(typ + ' '+str(timestamp) + ' ' +text+'\n')
-
-class Server:
-    def __init__(self):
-        self.socket = socket.socket()
-        self.port = 27526
-        self.ip = '127.0.0.1'
-        self.is_connected = False
-        log('!',f'Server IP {self.ip}')
-        log('!',f'Server PORT {self.port}')
+    with open('logfile.txt', 'a') as file:
+        file.write(typ + ' ' + str(timestamp) + ' ' + text + '\n')
 
 
-    def is_Active(self):
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('0.0.0.0', 27526))
+server.listen(5)
 
-        active=None         # Remaining
-        if active==1:
-            return True
-        else:
-            return False
-    
-    def forward (self, drop):
-        decypted_msg_list =Security.app_decrypt(drop[1])
-        
-        reciever_id=""
+while True:
+    print('Hello')
 
 
-    
-    def save_messg(self):
-        pass
-
-    def send (self, drop):
-        pass
-        
+# class Server:
+#     def __init__(self):
+#         self.server = 'None'
+#         self.create_socket()
+#
+#
+#         #     client_socket
+#             # self.clientport = 27526
+#         # self.clientip = '127.0.0.1'
+#         # self.is_connected = False
+#         # log('!',f'Server IP {self.ip}')
+#         # log('!',f'Server PORT {self.port}')
+#
+#     def create_socket(self):
+#         try:
+#             self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#             self.server.bind(('0.0.0.0', 27526))
+#             self.server.listen(5)
+#             # while True:
+#             #     message = self.server.recv(1024)
+#             #     print(message)
+#
+#         except:
+#             log('-', f'Error: unable to create socket')
+#             exit(-1)
+#
+#     def is_Active(self):
+#
+#         active = None  # Remaining
+#         if active == 1:
+#             return True
+#         else:
+#             return False
+#
+#     def send(self, drop):
+#         pass
+#         # clientsocket.send((pickle.dumps(drop))
+#
+#     def forward(self, drop):
+#         pass
+#         #decypted_msg_list = Security.app_decrypt(drop[1])
+#         # reciever_id=""
+#         # send(decypted_msg_list)
+#
+#     def save_messg(self):
+#         pass
+#
+#     def recieve(self):
+#         pass
+#
+# if __name__ == '__main__':
+#     serv = Server()
+#
